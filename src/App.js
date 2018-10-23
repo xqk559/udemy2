b7a620c24875d087232aabc238732167bf7d4741
keith <xqk559@gmail.com>
2018-10-23 12:01:04 -0400

    1.1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,96 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
 
 state = {
   StateText: '',
   Outs: 'Outs',
-  showCharComponent: false
+  showChar: true
 }
+
 textHandler = (event) => {
 this.setState( {
   StateText: event.target.value,
-  length: null
+  length: null,
 })
 }
+
+ClickHandler = (event) => {
+  this.setState( {
+    showChar: false
+  });
+}
+
 
   render() {
 
 const style = {
+  backgroundColor: 'green',
   display: 'inline-block',
+  color: 'white',
   padding: '16px',
-  textalign: 'center',
+  textAlign: 'center',
   margin: '16 px',
-  border: '1px solid black'
+  border: '1px solid black',
+  ':hover':{
+    backgroundColor: 'lightgreen',
+    color: 'black'
+  }
 };
 
-  let Text = 'Hello' ;
+
+
+
+let Text = 'Hello' ;
+
+this.state.StateText.split('');
+
+
+let CharComponent = this.state.StateText.split('');
 
 let Outs = '';
+
+let Array1 = Array.from(this.state.StateText);
+
+let DeleteCharHandler = (index) => {
+  const Text = this.state.StateText.split('');
+  Text.splice(index, 1);
+  const updatedText = Text.join('');
+  this.setState({StateText: updatedText});
+}
+
+const Char = (props) => {
+    const style = {
+      '@media (min-width 500px)': {
+        width: '450px'
+      }
+    }
+
+  return (<div style ={style } onClick={props.click} key="key5">
+    {props.character}
+
+    </div>)
+};
+
+const CharList = this.state.StateText.split('').map((ch, index) => {
+  return (<Char
+            character ={ch}
+            key = {index}
+            click={() => DeleteCharHandler(index)}
+            />)
+});
+
+
+
+
+
+if (this.state.showCharComponent === true) {
+  let CharComponent = this.state.StateText[0];
+}
+
 
 let validation = () => {
   if (this.state.StateText.length >= 5) {
@@ -36,28 +98,55 @@ let validation = () => {
       <div>
        The text is long enough.
       </div>
-    )}
+    )
+style.backgroundColor = 'red';
+style.hover = {
+  backgroundColor: 'salmon',
+  color: 'black'
+}
+
+  }
     else {
     Outs = (
       <div>
-       The text is not long enough.
+       The text is not long enough, motherfucker.
       </div>
   )}
 }
 
-let deleteInstance = () => {
+let classes = [];
+if (this.state.StateText.length <=2){
+  classes.push('red');
+}
+if(this.state.StateText.length<=1){
+  classes.push('bold');
+}
 
+
+
+let Cha = '';
+
+let shower = () => {
+  if (this.state.showChar === true) {
+    Cha = (<div   key="key1">{CharList}</div>)
+  }
+  else {
+    Cha = (<div>nothing</div>);
+  }
 }
 
 
     return (
+<StyleRoot>
+
 
       <div className="App">
 
           <input type="text" onChange={this.textHandler}/>
           <br></br>
+          <nf>{shower()}</nf>
           <np>{validation()}</np>
-          <np>{Text}</np>
+          <np></np>
           <br></br>
           <Text
             change = {this.textHandler}
@@ -65,17 +154,34 @@ let deleteInstance = () => {
             click = {() => this.deleteInstance}
             />
           <br></br>
-          <np style = {style}>{this.state.StateText}</np>
+          <np style = {style}  key="key2">{this.state.StateText}</np>
+          <br></br>
+          <div style = {style} key="key6">Hardstyles!</div>
+          <br></br>
+          <np className={classes.join(' ')}>Christmas Text!</np>
+          <br></br>
           <br></br>
           <np >{this.state.StateText.length}</np>
           <br></br>
-          <mp style = {style}>{Outs}</mp>
+          <mp style = {style}  key="key3">{Outs}</mp>
           <br></br>
           <np>{this.state.StateText[0]}</np>
+          <br></br>
+          <button onClick={this.ClickHandler}>This Motherfucker Right Here</button>
+          <br></br>
+          <np style = {style} key="key4">{CharList}</np>
+          <br></br>
+          <br></br>
+          <p onClick={this.ClickHandler}>{Cha}
+          </p>
+          <np>{Char}</np>
 
       </div>
+
+
+</StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
